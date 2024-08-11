31ccc97c0c223699227dcdcb68fef2e5a58f8987
helloworld-hkm <hakimastina@gmail.com>
2024-08-11 15:32:16 +0700

    learn#2:navigate and hover

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',\App\Livewire\Home::class);
-Route::get('/about',\App\Livewire\About::class);
-Route::get('/posts',\App\Livewire\Posts\Index::class);
+Route::get('/',\App\Livewire\Home::class)->name('home');
+Route::get('/about',\App\Livewire\About::class)->name('about');
+Route::get('/posts',\App\Livewire\Posts\Index::class)->name('posts.index');
 
 Route::get('users/{user}',\App\Livewire\Users\Show::class)->name('users.show');
 

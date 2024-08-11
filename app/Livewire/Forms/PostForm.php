4ed7feba01ae5c2aa521d025569a5026c72d04ea
helloworld-hkm <hakimastina@gmail.com>
2024-08-11 16:39:05 +0700

    Learn#4:form

---
+++ app/Livewire/Forms/PostForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use App\Models\User;
+use Livewire\Attributes\Rule;
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+
+class PostForm extends Form
+{
+    #[Rule(['required','string','min:3','max:255'])]
+    public $title='';
+    #[Rule(['required'])]
+    public $body='';
+    public function store(){
+        $user= User::find(1); //TODO:make auth
+
+        $validated=$this->validate();
+        $user->posts()->create($validated);
+        $this->reset();
+    }
+}
